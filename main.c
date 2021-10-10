9fc74c84cb81959fd7ebe7ee92f5ce525dc48d24
CrazyBolillo <40015596+CrazyBolillo@users.noreply.github.com>
2021-10-09 22:21:54 -0500

    Forgot what changes I made. But I made some

--- main.c
+++ main.c
@@ -14,8 +14,8 @@
 #pragma config FOSC = 101
 
 #define _XTAL_FREQ 8000000
-#define TMR1_START_H 0xF7
-#define TMR1_START_L 0xDA
+#define TMR1_START_H 0xFD
+#define TMR1_START_L 0xE8
 
 #define DISPLAY PORTC
 #define DISPLAY_SELECT PORTD
@@ -24,14 +24,26 @@
 #define FAN_2 PORTDbits.RD5
 #define HEATER_1 PORTDbits.RD6
 #define HEATER_2 PORTDbits.RD7
+#define ALARM PORTEbits.RE1
+
+#define BUZZER_HOT_START 230
+#define BUZZER_COLD_START 15
+
+#define TMR2_OFF T2CON &= 0xFB
+#define TMR2_ON T2CON |= 0x04
 
 const uint8_t led_numbers[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 
     0x7F, 0x67, 0x77};
 uint8_t display_position = 0;
 
-const double resolution = 48.8758533;
+const double resolution = 32.258064;
 uint16_t adc_value = 0;
-uint16_t temperature = 2025;
+uint16_t temperature = 0;
+
+// 0 is for buzzing when HOT, 1 is for buzzing when cold.
+uint8_t buzzer_type = 0;
+uint8_t buzz_state = 0;
+uint16_t buzz_count = 0;
 
 void read_adc() 
 {
@@ -70,7 +82,7 @@ void display_number()
 
 void __interrupt() handle_int(void)
 {
-    if ((PIE1bits.TMR1IE == 1) && (PIR1bits.TMR1IF == 1)) {
+    if (PIR1bits.TMR1IF == 1) {
         T1CON &= 0xFE;
         
         display_number();
@@ -84,6 +96,21 @@ void __interrupt() handle_int(void)
         PIR1bits.TMR1IF = 0;
         T1CON |= 0x01;
     }
+    
+    if (PIR1bits.TMR2IF == 1) {
+        TMR2_OFF;
+        buzz_state = !buzz_state;
+        ALARM = buzz_state;
+        if (buzzer_type == 0) {
+            PR2 = BUZZER_HOT_START;
+        }
+        else {
+            PR2 = BUZZER_COLD_START;
+        }
+        
+        PIR1bits.TMR2IF = 0;
+        TMR2_ON;
+    }
 }
 
 void main(void) 
@@ -95,44 +122,56 @@ void main(void)
     ADCON0 |= 0x01;
     
     PIE1bits.TMR1IE = 1;
+    PIE1bits.TMR2IE = 1;
     INTCONbits.PEIE = 1;
     INTCONbits.GIE = 1;
-    
+
     PORTC = 0x00;
     TRISC = 0x00;
     PORTD = 0x00;
     TRISD = 0x00;
-    TRISE = 0x0F;
-    
+    PORTE = 0x00;
+    TRISE = 0x0C;
+    T2CON = 0x79;
     T1CON = 0xB1;
+            
     while (1)
     {
         read_adc();
-        if (temperature >= 6000) {
+        __delay_ms(350);
+ 
+        if (temperature >= 4500) {
             FAN_1 = 1;
-            FAN_2 = 1;
+            if (temperature >= 6000) {
+                FAN_2 = 1;
+                buzzer_type = 0;
+                TMR2_ON;
+            }
+            if (temperature < 6000) {
+                FAN_2 = 0;
+                TMR2_OFF;
+            }
         }
-        else if (temperature >= 4500) {
-            FAN_1 = 1;
-            FAN_2 = 0;
-        }
-        else if (temperature < 4500) {
+        else {
             FAN_1 = 0;
-            FAN_2 = 0;
         }
-        
+        if (temperature <= 1500) {
+            HEATER_1 = 1;
+        }
         if (temperature > 1500) {
             HEATER_1 = 0;
-            HEATER_2 = 0;
         }
-        else if (temperature <= 1000) {
-            HEATER_1 = 1;
+        if (temperature <= 1000) {
             HEATER_2 = 1;
+            buzzer_type = 1;
+            TMR2_ON;
         }
-        else if (temperature <= 1500) {
-            HEATER_1 = 1;
+        if (temperature > 1000) {
             HEATER_2 = 0;
         }
+        if (temperature > 1000 && temperature < 1600) {
+            TMR2_OFF;
+        }   
     }
     
 }
